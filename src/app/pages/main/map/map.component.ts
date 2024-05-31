6b0f2af252872fd5f6e7741e8b17a89578f0ee41
Anastasia Demina <fioB7@yandex.ru>
2024-05-31 22:39:50 +0500

    map list of addresses

--- src/app/pages/main/map/map.component.ts
+++ src/app/pages/main/map/map.component.ts
@@ -12,9 +12,11 @@ export class MapComponent {
             properties: {
                 balloonContentBody: [
                     '<address>',
-                    '<strong>Шаурма-Ауфф</strong>',
+                    '<strong>Зал футбола</strong>',
                     '<br/>',
-                    'Дешевая и вкусная шаурма и прочие вкусности в виде выпечки',
+                    'ул. Мира, д. 35/а',
+                    '<br/>',
+                    'Зал для тренировок младшей группы',
                     '</address>',
                 ].join(''),
             }
@@ -24,12 +26,84 @@ export class MapComponent {
             properties: {
                 balloonContentBody: [
                     '<address>',
-                    '<strong>Столовая ГУК\'а</strong>',
+                    '<strong>Зал баскетбола</strong>',
                     '<br/>',
-                    'Не зря же здесь всегда полно народу: вкусно и дешево',
+                    'ул. Мира, д. 66',
+                    '<br/>',
+                    'Зал для тренировок старшей группы',
                     '</address>',
                 ].join(''),
             }
-        }
+        },
+        {
+            coords: [56.84155606786889,60.654337999999996],
+            properties: {
+                balloonContentBody: [
+                    '<address>',
+                    '<strong>Зал футбола</strong>',
+                    '<br/>',
+                    'ул. Мира, д. 35/а',
+                    '<br/>',
+                    'Зал для тренировок младшей группы',
+                    '</address>',
+                ].join(''),
+            }
+        },
+        {
+            coords: [56.83989636680168,60.65098611780828],
+            properties: {
+                balloonContentBody: [
+                    '<address>',
+                    '<strong>Зал баскетбола</strong>',
+                    '<br/>',
+                    'ул. Мира, д. 66',
+                    '<br/>',
+                    'Зал для тренировок старшей группы',
+                    '</address>',
+                ].join(''),
+            }
+        },
+        {
+            coords: [56.842497067871264,60.643791499999985],
+            properties: {
+                balloonContentBody: [
+                    '<address>',
+                    '<strong>Зал футбола</strong>',
+                    '<br/>',
+                    'ул. Мира, д. 35/а',
+                    '<br/>',
+                    'Зал для тренировок младшей группы',
+                    '</address>',
+                ].join(''),
+            }
+        },
+        {
+            coords: [56.841788449199086,60.637593682882255],
+            properties: {
+                balloonContentBody: [
+                    '<address>',
+                    '<strong>Зал баскетбола</strong>',
+                    '<br/>',
+                    'ул. Мира, д. 66',
+                    '<br/>',
+                    'Зал для тренировок старшей группы',
+                    '</address>',
+                ].join(''),
+            }
+        },
+        {
+            coords: [56.83837827306169,60.63821523065366],
+            properties: {
+                balloonContentBody: [
+                    '<address>',
+                    '<strong>Зал футбола</strong>',
+                    '<br/>',
+                    'ул. Мира, д. 35/а',
+                    '<br/>',
+                    'Зал для тренировок младшей группы',
+                    '</address>',
+                ].join(''),
+            }
+        },
     ];
 }
--- src/app/pages/main/map/map.module.ts
+++ src/app/pages/main/map/map.module.ts
@@ -1,7 +1,7 @@
 import { AngularYandexMapsModule, YaConfig } from 'angular8-yandex-maps';
 import { MapComponent } from "./map.component";
 import { NgModule } from "@angular/core";
-import { NgForOf } from "@angular/common";
+import { NgForOf, NgIf } from "@angular/common";
 
 const mapConfig: YaConfig = {
     apikey: '8c2cdcb1-d99b-4933-bae4-58cac30e0d02',
@@ -14,7 +14,8 @@ const mapConfig: YaConfig = {
     ],
     imports: [
         AngularYandexMapsModule.forRoot(mapConfig),
-        NgForOf
+        NgForOf,
+        NgIf
     ],
     exports: [
         MapComponent
