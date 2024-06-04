9ee2f4044fa6a4cd4be8b05fce87706be2757801
Misha <gmike103g@gmail.com>
2024-06-04 22:29:46 +0500

    fix header

--- src/app/pages/main/layout/header/header.component.ts
+++ src/app/pages/main/layout/header/header.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { NAVIGATION_ITEMS } from './header-navigation';
-import { ContainerComponent } from '../../../../shared/components/conteiner/container.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    ContainerComponent
-  ],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
